248b547d6bb87f2a87b85fa896c702462868b37a
Natan Foleto <natanfoleto2015@hotmail.com>
2023-08-03 22:43:16 -0300

    Create Comment and use phosphor-icons

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,48 +1,66 @@
 import Avatar from "../Avatar";
+import Comment from "../Comment";
 
-import { Container, Content, Hashtags, Divider } from "./styles";
+import {
+  Container,
+  Header,
+  Author,
+  AuthorInfo,
+  Content,
+  Description,
+  Hashtags,
+  Divider,
+  CommentForm,
+  Comments,
+} from "./styles";
 
 const Post: React.FC = () => {
   return (
     <Container>
-      <header>
-        <div>
+      <Header>
+        <Author>
           <Avatar />
 
-          <section>
+          <AuthorInfo>
             <h1>Natan Foleto</h1>
             <p>natanfoleto@hotmail.com</p>
-          </section>
-        </div>
+          </AuthorInfo>
+        </Author>
 
-        <p>Publicado à 1h</p>
-      </header>
+        <time>Publicado à 1h</time>
+      </Header>
 
-      <main>
-        <Content>
+      <Content>
+        <Description>
           <p>Fala galeraa 👋</p>
           <p>
             Você pode sempre sonhar, e seus sonhos se tornarão sonhos, e a
             realidade se tornará realidade. Mas é você que tem que torná-los
             realidade. 🚀
           </p>
-        </Content>
+        </Description>
 
         <Hashtags>
           <span>#collabspace</span>
           <span>#confia</span>
         </Hashtags>
-      </main>
+      </Content>
 
       <Divider />
 
-      <footer>
+      <CommentForm>
         <h1>Deixe seu comentário</h1>
 
         <textarea name=""></textarea>
 
         <button>Comentar</button>
-      </footer>
+      </CommentForm>
+
+      <Divider />
+
+      <Comments>
+        <Comment />
+      </Comments>
     </Container>
   );
 };
