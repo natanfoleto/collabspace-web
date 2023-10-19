9eb88593b10150951fc6408f0aa1eaba9e9d6e3d
Natan Foleto <natanfoleto2015@hotmail.com>
2023-10-19 19:55:30 -0300

    Update class 19/10

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,28 @@
 import { useNavigate } from "react-router-dom";
 
-import { SignOut } from "phosphor-react";
+import { SignOut, House } from "phosphor-react";
 
 import { useAuthentication } from "../../contexts/Authentication";
 
 import { LogoRocket } from "../../assets/sources";
 
-import { Container, Navbar, Aside, AsideButton } from "./styles";
+import AvatarCircle from "../AvatarCircle";
+
+import {
+  Container,
+  Brand,
+  Navbar,
+  Menu,
+  Item,
+  Aside,
+  AsideButton,
+  Divider,
+} from "./styles";
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
 
-  const { signOut } = useAuthentication();
+  const { user, me, signOut } = useAuthentication();
 
   const handleLogo = () => {
     navigate("/feed");
@@ -19,15 +30,31 @@ const Header: React.FC = () => {
 
   return (
     <Container>
-      <LogoRocket onClick={handleLogo} />
+      <Brand>
+        <LogoRocket onClick={handleLogo} />
+      </Brand>
 
-      <Navbar>NAVBAR</Navbar>
+      <Navbar>
+        <Menu>
+          <Item>
+            <House size={28} weight="fill" />
+          </Item>
+        </Menu>
 
-      <Aside>
-        <AsideButton title="Sair" onClick={signOut}>
-          <SignOut size={22} weight="fill" />
-        </AsideButton>
-      </Aside>
+        <Aside>
+          <AsideButton title="Sair" onClick={signOut}>
+            <SignOut size={18} weight="fill" />
+          </AsideButton>
+
+          <Divider />
+
+          <AvatarCircle
+            size="40px"
+            avatar={user?.avatarUrl}
+            onClick={() => me(user?.id)}
+          />
+        </Aside>
+      </Navbar>
     </Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,12 +2,13 @@ import { styled } from "styled-components";
 
 export const Container = styled.header`
   width: 100vw;
-  height: 5rem;
+  height: 4rem;
   background: var(--zinc-800);
 
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  gap: 2rem;
+
   padding: 0 2rem;
 
   position: fixed;
@@ -20,9 +21,52 @@ export const Container = styled.header`
   color: white;
 `;
 
-export const Navbar = styled.nav``;
+export const Brand = styled.div`
+  width: 25%;
 
-export const Aside = styled.aside``;
+  display: flex;
+  align-items: center;
+`;
+
+export const Navbar = styled.nav`
+  width: 75%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  svg {
+    color: var(--emerald-600);
+  }
+`;
+
+export const Menu = styled.div`
+  height: 100%;
+
+  display: flex;
+`;
+
+export const Item = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
+  padding: 0 2rem;
+  background: transparent;
+  outline: 0;
+  border: 0;
+
+  border-bottom: 3px solid var(--emerald-600);
+`;
+
+export const Aside = styled.aside`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  gap: 8px;
+`;
 
 export const AsideButton = styled.button`
   display: flex;
@@ -45,4 +89,11 @@ export const AsideButton = styled.button`
   &:hover {
     background: var(--zinc-600);
   }
+`;
+
+export const Divider = styled.div`
+  width: 1px;
+  height: 2rem;
+  background: var(--zinc-600);
+  margin: 0 4px;
 `;
